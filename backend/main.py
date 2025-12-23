541ba5f839c0788e09b67d199452d551eaa0d55f
Omkar Solanke <110219809+omkarsolanke@users.noreply.github.com>
2025-12-23 13:55:33 +0530

    Add additional allowed origins for CORS

--- backend/main.py
+++ backend/main.py
@@ -38,6 +38,9 @@ app = FastAPI(title="QuickServe API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://127.0.0.1:8081",
+        "http://localhost:19006",
+        "http://localhost:8081",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://192.168.1.2:5173",
